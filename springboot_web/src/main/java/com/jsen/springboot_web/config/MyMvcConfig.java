cae4e0c5d7fd5f28d70092fb30e4935e4271dd9c
jhs <724033319@qq.com>
2020-05-11 14:47:41 +0800

    自定义 视图解析器

--- springboot_web/src/main/java/com/jsen/springboot_web/config/MyMvcConfig.java
+++ springboot_web/src/main/java/com/jsen/springboot_web/config/MyMvcConfig.java
@@ -1,13 +1,12 @@
 package com.jsen.springboot_web.config;
 
 
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * 扩展
+ * 自定义视图解析器
  *
  * @author Jsen on 2020/5/7
  */
@@ -15,6 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/atJsen").setViewName("index");
+        registry.addViewController("/atJsen").setViewName("index");//访问/atjsen 将跑到index页面
     }
 }
