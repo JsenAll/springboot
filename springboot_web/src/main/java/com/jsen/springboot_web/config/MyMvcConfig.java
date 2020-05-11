9bf46a89ec33ce457c382d493bf48e41070596e4
jhs <724033319@qq.com>
2020-05-11 18:04:40 +0800

    国际化

--- springboot_web/src/main/java/com/jsen/springboot_web/config/MyMvcConfig.java
+++ springboot_web/src/main/java/com/jsen/springboot_web/config/MyMvcConfig.java
@@ -1,7 +1,10 @@
 package com.jsen.springboot_web.config;
 
 
+import com.jsen.springboot_web.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -15,5 +18,13 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/atJsen").setViewName("index");//访问/atjsen 将跑到index页面
+        registry.addViewController("/index.html").setViewName("index");
     }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
+    }
+
+
 }
