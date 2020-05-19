091a24c9a69b59cce0440583fd5f9f6cf484a1a8
jhs <724033319@qq.com>
2020-05-19 18:18:56 +0800

    up

--- springboot_web/src/main/java/com/jsen/springboot_web/controller/EmployeeController.java
+++ springboot_web/src/main/java/com/jsen/springboot_web/controller/EmployeeController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -23,6 +25,7 @@ public class EmployeeController {
     @Autowired
     DepartmentDao departmentDao;
 
+    //查询全部员工
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> all = employeeDao.getAll();
@@ -30,11 +33,35 @@ public class EmployeeController {
         return "emp/list";
     }
 
+    //添加员工页面
     @GetMapping("/emp")
     public String add(Model model) {
-        Collection<Employee> all = employeeDao.getAll();
+
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("deps", departments);
         return "emp/add";
     }
+
+    //添加员工到数据库
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        System.out.println(employee.toString());
+        employeeDao.save(employee);
+        //forward:
+        return "redirect:/emps";
+    }
+
+//    @GetMapping("/emp/{id}")
+//    public String toEditPage(@PathVariable("id") Integer id, Model model) {
+////        Employee employee = employeeDao.get(id);
+////        model.addAttribute("emp", employee);
+//        return "emp/add";
+//
+//    }
+     @GetMapping("/emp/{id}")
+    public String toEditPage() {
+        return "emp/add";
+    }
+
+
 }
